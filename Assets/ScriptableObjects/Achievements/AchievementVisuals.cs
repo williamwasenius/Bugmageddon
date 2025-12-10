b433762f58bbee7b081af05ed91db39fae5bb2fd
Diana <sandu.diana2001@gmail.com>
2025-12-10 13:03:22 +0200

    fixed

--- Assets/ScriptableObjects/Achievements/AchievementVisuals.cs
+++ Assets/ScriptableObjects/Achievements/AchievementVisuals.cs
@@ -1,20 +1,52 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class AchievementVisuals : MonoBehaviour
 {
-    public AchievementSO[] achievements;
+    public AchievementSO[] achievementSOs;
     public GameObject achievementUIPrefab;
     public Transform parent;
     public void Start()
     {
-        for(int i = 0; i <achievements.Length; i++)
+        SetSOCompleted();
+
+        for(int i = 0; i <achievementSOs.Length; i++)
         {
-            var ui = Instantiate(achievementUIPrefab, new Vector2(300, 400 + i*-150), Quaternion.identity, parent);
+            var ui = Instantiate(achievementUIPrefab, new Vector2(975, 650 + i*-150), Quaternion.identity, parent);
             AchivementUI script = ui.GetComponent<AchivementUI>();
-            script.Setup(achievements[i]);
+            script.Setup(achievementSOs[i]);
         }
 
         RectTransform parentRT = parent.GetComponent<RectTransform>();
-        parentRT.sizeDelta = new Vector2(parentRT.sizeDelta.x, achievements.Length * 150 + 250);
+        parentRT.sizeDelta = new Vector2(parentRT.sizeDelta.x, achievementSOs.Length * 150 + 250);
+    }
+
+    private void SetSOCompleted()
+    {
+        var achivSave = AchievementManager.Instance;
+        if (achivSave.tutorialAchievement)
+        {
+            achievementSOs[0].unlocked = true;
+        }
+        if (achivSave.mission1Achievement)
+        {
+            achievementSOs[1].unlocked = true;
+        }
+        if (achivSave.mission2Achievement)
+        {
+            achievementSOs[2].unlocked = true;
+        }
+        if (achivSave.mission3Achievement)
+        {
+            achievementSOs[3].unlocked = true;
+        }
+        if (achivSave.mission4Achievement)
+        {
+            achievementSOs[4].unlocked = true;
+        }
+        if (achivSave.mission5Achievement)
+        {
+            achievementSOs[5].unlocked = true;
+        }
     }
 }
